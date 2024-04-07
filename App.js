4cc8666b68116e44c6262c8047ef8c48e2b792d3
Revanth-Kumar-Vennu <revanthkumarvennu@gmail.com>
2024-04-06 22:26:19 -0400

    logging added

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ import "dotenv/config";
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 console.log(CONNECTION_STRING);
+console.log(process.env.FRONTEND_URL)
 mongoose.connect(CONNECTION_STRING);
-// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(
   cors({
